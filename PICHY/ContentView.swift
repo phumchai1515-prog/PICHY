f467bb2168df20710577de71fc94d44a28a2822a
Phumchai Tanonsi <274848436+phumchai1515-prog@users.noreply.github.com>
2026-06-22 20:30:42 +0700

    Initial Commit

---
+++ PICHY/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PICHY
+//
+//  Created by Phumchai Tanonsi on 22/6/2569 BE.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PICHY/PICHYApp.swift
@@ -0,0 +1,17 @@
+//
+//  PICHYApp.swift
+//  PICHY
+//
+//  Created by Phumchai Tanonsi on 22/6/2569 BE.
+//
+
+import SwiftUI
+
+@main
+struct PICHYApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
